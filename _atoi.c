d3224e174ecb851d6de28a959fb3101addad5797
hindoz <hindamir.99@gmail.com>
2023-10-20 16:17:47 -0700

    edit_atoi

--- _atoi.c
+++ _atoi.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * isnumb - check for numbers
+  * isnum - check for numbers
   * @b: character to test
   * Return: 1 if number 0 otherwise
   */
-int isnumb(char b)
+int isnum(char b)
 {
 	if (b >= '0' && b <= '9')
 		return (1);
@@ -13,17 +13,17 @@ int isnumb(char b)
 }
 
 /**
-  * hypen - check number of minus signs
+  * hyphens - check number of minus signs
   * @str: string to check
   * @i: where the iteration stopped
   * Return: number of hyphens
   */
-int hypen(char *str, int *i)
+int hyphens(char *str, int *i)
 {
 	char *str1 = str;
 	int summ = 0;
 
-	while (!isnumb(*str1) && *str1 != '\0')
+	while (!isnum(*str1) && *str1 != '\0')
 	{
 		(*i)++;
 		if (*str1++ == '-')
@@ -43,14 +43,14 @@ int _atoi(char *str)
 	unsigned int answ = 0;
 	char *a = str;
 
-	num = hypen(a, &j);
+	num = hyphens(a, &j);
 
 	a += j;
-	while (isnumb(*++a) && *a != '\0')
+	while (isnum(*++a) && *a != '\0')
 		ml *= 10;
 
 	str += j;
-	while (isnumb(*str) && *str != '\0')
+	while (isnum(*str) && *str != '\0')
 	{
 		answ += (*str++ - '0') * ml;
 		ml /= 10;
